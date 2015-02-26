6f3ef8f86b6bc5803c09daaf602756149ef82360
Benjamin Gorman <8076bgorman@gmail.com>
2015-02-26 23:23:25 +0000

    add function to test if working tree is clean

--- run_email_updater.sh
+++ run_email_updater.sh
@@ -1,5 +1,36 @@
 #!/bin/bash
+require_clean_work_tree () {
+    git rev-parse --verify HEAD >/dev/null || exit 1
+    git update-index -q --ignore-submodules --refresh
+    err=0
 
-if git diff-index --quiet HEAD --; then
-    echo "There are local changes!"    
+    if ! git diff-files --quiet --ignore-submodules
+    then
+        echo >&2 "Cannot $1: You have unstaged changes."
+        err=1
+    fi
+
+    if ! git diff-index --cached --quiet --ignore-submodules HEAD --
+    then
+        if [ $err = 0 ]
+        then
+            echo >&2 "Cannot $1: Your index contains uncommitted changes."
+        else
+            echo >&2 "Additionally, your index contains uncommitted changes."
+        fi
+        err=1
+    fi
+
+    if [ $err = 1 ]
+    then
+        test -n "$2" && echo >&2 "$2"
+        exit 1
+    fi
+}
+
+if [ require_clean_work_tree ]
+then
+    echo "Work tree is clean"
+else
+    echo "Work tree is not clean"
 fi
