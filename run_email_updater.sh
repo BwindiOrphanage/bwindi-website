d10d04ae93e2fa96d5d218f997282c35f2cd448e
Benjamin Gorman <8076bgorman@gmail.com>
2015-02-26 23:49:12 +0000

    Add success test to email updater script

--- run_email_updater.sh
+++ run_email_updater.sh
@@ -5,8 +5,13 @@ if git diff-index --quiet HEAD --; then
     python2 email_updater.py $GMAIL_USERNAME $GMAIL_PASSWORD
     git add .
     git commit -m "AUTOMATED: Adding new blog post files."
-    git push origin master
-    exit 0
+    git push origin gh-pages
+    if [ $? -eq 0 ]
+        echo "Commit successful."
+        exit 0
+    else
+        echo "Commit not successful."
+        exit 1
 else
     # changes
     echo "Changes present in repo, not continuing."
